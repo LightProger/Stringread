627da7d9c5c2812dc591d39652f79edd43560ffa
Alexandr <photoluxe@mail.ru>
2017-02-19 17:59:02 +0600

    Чтение из файла

---
+++ src/Alexandr_Nikitchenko/StringRead.java
@@ -0,0 +1,19 @@
+package Alexandr_Nikitchenko;
+
+import java.io.*;
+public class StringRead extends Exception {
+
+    //Чтение из файла
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Александр.Acer\\Documents\\GitHub" +
+                "\\Stringread\\src\\Alexandr_Nikitchenko\\Read.txt"));
+       // null - конец файла. Пока не достигнут конец файла, считывать строки и выводить на печать
+        while(true){
+            String line = br.readLine();
+            if (line == null) break;
+            System.out.println(line);
+        }
+
+    }
+}
